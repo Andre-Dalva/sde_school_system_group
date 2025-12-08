ae68557129e532d1d2accdb53263123e9890d30d
André d'Alva <andreluiscapela@gmail.com>
2025-12-08 04:48:30 +0100

    Something for Frontend

--- Frontend/scripts/studentUserScripts/userInfo.js
+++ Frontend/scripts/studentUserScripts/userInfo.js
@@ -1,12 +1,24 @@
 const token = localStorage.getItem("token");
+
 fetch("https://invaluably-grapier-jeni.ngrok-free.dev/users/me", {
     method: "GET",
     headers: {
         "Authorization": "Bearer " + token,
         "ngrok-skip-browser-warning": "true"
     }
-}).then(response => response.json()).then(data => {
-    console.log(data)
-    const welcomeUser = document.getElementById("helloUsername");
-    welcomeUser.innerText = data.name;
-}).catch(err => console.log(err))
+})
+.then(response => response.json()) 
+.then(userData => {
+    displayInfo(userData); 
+})
+.catch(err => {
+    console.error("Failed to fetch user data:", err);
+});
+
+function displayInfo(userData){
+    document.getElementById("displayFullName").innerText = userData.name;
+    document.getElementById("displayUsername").innerText = userData.username
+    document.getElementById("displayEmail").innerText = userData.email
+    document.getElementById("displayBirth").innerText = userData.birthDate;
+    document.getElementById("displayRole").innerText = userData.role;
+}
